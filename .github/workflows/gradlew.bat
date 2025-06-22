d8288a4c13f942d3b20dee76c2de99e4f7c894dd
Awan213 <99831157+Awan213@users.noreply.github.com>
2025-06-22 22:31:13 +0700

    Delete .github/workflows/gradlew.bat

--- .github/workflows/gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set DIR=%~dp0
-set APP_HOME=%DIR%
-set JAVA_EXE=java
-if defined JAVA_HOME (
-  set JAVA_EXE=%JAVA_HOME%\bin\java
-)
-"%JAVA_EXE%" -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
