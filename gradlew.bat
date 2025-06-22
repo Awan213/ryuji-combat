9ec3e913ed26634765db13fc97aa6ae7b9d35f66
Awan213 <99831157+Awan213@users.noreply.github.com>
2025-06-22 23:35:57 +0700

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Gradle start script for Windows
-
-set DIR=%~dp0
-
-set JAVA_EXE=java
-if defined JAVA_HOME set JAVA_EXE=%JAVA_HOME%\bin\java
-
-"%JAVA_EXE%" -cp "%DIR%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
